470c9069098f8373fe2518f54f2d9a628abe4881
asobolev <sobolev.andrey@gmail.com>
2015-07-10 17:08:32 +0200

    parser class draft for gdrive

--- gdrive/collector.py
+++ gdrive/collector.py
@@ -2,21 +2,60 @@ import json
 import gspread
 
 from oauth2client.client import SignedJwtAssertionCredentials
+from rdflib import Graph, Literal, BNode, Namespace, RDF, URIRef
+from rdflib.namespace import DC, FOAF
 
 
-json_key = json.load(open('Sirota-lab-f122faf9d28f.json'))
-scope = ['https://spreadsheets.google.com/feeds']
+class Parser(object):
+    """
+    Class that downloads data from a remote Google Table file and parses it
+    to RDF.
+    """
+    base_ont = "../base.owl"
+    custom_ont = "../custom.owl"
 
-credentials = SignedJwtAssertionCredentials(
-    json_key['client_email'],
-    bytes(json_key['private_key'], 'utf-8'),
-    scope
-)
+    owl_cls = 'http://www.w3.org/2002/07/owl#Class'
 
-gc = gspread.authorize(credentials)
+    scope = ['https://spreadsheets.google.com/feeds']
 
-f = gc.open("DATA-2015-DEV")
+    def __init__(self, path_to_credentials, filename):
+        self.credentials = path_to_credentials
+        self.filename = filename
+        self.f = None
 
-animal_sheet = f.worksheet("Animal")
-foo = animal_sheet.get_all_values()  # matrix of values
+    def connect(self):
+        json_key = json.load(open(self.credentials))
 
+        credentials = SignedJwtAssertionCredentials(
+            json_key['client_email'],
+            bytes(json_key['private_key'], 'utf-8'),
+            Parser.scope
+        )
+
+        gc = gspread.authorize(credentials)
+        self.f = gc.open("DATA-2015-DEV")
+
+    def parse(self):
+
+        # parse base ontology
+        g = Graph()
+        g.parse(Parser.base_ont, format="application/rdf+xml")
+
+        cls_triples = g.triples((None, RDF.type, URIRef(Parser.owl_cls)))
+        cls_list = [s for s, p, o in cls_triples if not type(s) == BNode]
+        cls_names = [x.split("#")[1] for x in cls_list]
+
+        for sheet in self.f.worksheets():
+            if sheet.title in cls_names:
+                # class is found in ontology
+                pass
+
+            else:
+                # should be saved in custom ontology
+                pass
+
+            # data = sheet.get_all_values()  # matrix of values
+
+
+class Fuser(object):
+    pass
